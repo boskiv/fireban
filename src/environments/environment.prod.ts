a1fb5ac457e69c857d23cd4aea6025170e64b065
Ivan Skiridomov <ivan.skiridomov@gmail.com>
2020-02-18 15:35:44 -0600

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAkjhSRxRksWV_rx9rK2lt7GBkXrZwS46U',
+    authDomain: 'fireban-f1870.firebaseapp.com',
+    databaseURL: 'https://fireban-f1870.firebaseio.com',
+    projectId: 'fireban-f1870',
+    storageBucket: 'fireban-f1870.appspot.com',
+    messagingSenderId: '978174761685',
+    appId: '1:978174761685:web:f173d674f77a9b806fe05c',
+    measurementId: 'G-YJ54GWX7PB'
+  }
+}
+;
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAkjhSRxRksWV_rx9rK2lt7GBkXrZwS46U',
+    authDomain: 'fireban-f1870.firebaseapp.com',
+    databaseURL: 'https://fireban-f1870.firebaseio.com',
+    projectId: 'fireban-f1870',
+    storageBucket: 'fireban-f1870.appspot.com',
+    messagingSenderId: '978174761685',
+    appId: '1:978174761685:web:f173d674f77a9b806fe05c',
+    measurementId: 'G-YJ54GWX7PB'
+  }
 };
 
 /*
