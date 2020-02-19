1201d13e4b88ce55a0814418c33c2fc3a2927e98
Ivan Skiridomov <ivan.skiridomov@gmail.com>
2020-02-19 08:41:50 -0600

    add fire guard, kanaban modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,27 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {HomePageComponent} from './home-page/home-page.component';
+import {canActivate, redirectLoggedInTo, redirectUnauthorizedTo} from '@angular/fire/auth-guard';
 
-
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
+const redirectLoggedInToKanban = () => redirectLoggedInTo(['kanban']);
 const routes: Routes = [
-  { path: '', component: HomePageComponent },
-  { path: 'login', loadChildren: () => import('./user/user.module').then(m => m.UserModule)}
-  ];
+  {path: '', component: HomePageComponent},
+  {
+    path: 'login',
+    loadChildren: () => import('./user/user.module').then(m => m.UserModule),
+    ...canActivate(redirectLoggedInToKanban)
+  },
+  {
+    path: 'kanban',
+    loadChildren: () => import('./kanban/kanban.module').then(m => m.KanbanModule),
+    ...canActivate(redirectUnauthorizedToLogin)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
